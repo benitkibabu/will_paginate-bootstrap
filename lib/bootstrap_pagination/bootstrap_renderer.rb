7b8377ed89be12fb64d7a5d2eec82c45fdbbee41
Benit Kibabu <benitkibabu@gmail.com>
2018-06-04 13:21:00 +0100

    changed class name for ul li

--- lib/bootstrap_pagination/bootstrap_renderer.rb
+++ lib/bootstrap_pagination/bootstrap_renderer.rb
@@ -28,9 +28,9 @@ module BootstrapPagination
       link_options = @options[:link_options] || {}
 
       if page == current_page
-        tag("li", tag("span", page), class: "page-item active")
+        tag("li", tag("span", page), class: " page-item active")
       else
-        tag("li", link(page, page, link_options.merge(rel: rel_value(page)), class: "page-link"), class: "page-item")
+        tag("li", link(page, page, link_options.merge(rel: rel_value(page)), class: " page-link "), class: " page-item ")
       end
     end
 
@@ -38,24 +38,24 @@ module BootstrapPagination
       link_options = @options[:link_options] || {}
 
       if page
-        tag("li", link(text, page, link_options), class: classname)
+        tag("li", link(text, page, link_options), class: classname % " page-link")
       else
-        tag("li", tag("span", text), class: "%s disabled" % classname)
+        tag("li", tag("span", text), class: "%s page-item disabled" % classname)
       end
     end
 
     def gap
-      tag("li", tag("span", ELLIPSIS), class: "disabled")
+      tag("li", tag("span", ELLIPSIS), class: "page-item disabled")
     end
 
     def previous_page
       num = @collection.current_page > 1 && @collection.current_page - 1
-      previous_or_next_page(num, @options[:previous_label], "page-item")
+      previous_or_next_page(num, @options[:previous_label], " page-item ")
     end
 
     def next_page
       num = @collection.current_page < @collection.total_pages && @collection.current_page + 1
-      previous_or_next_page(num, @options[:next_label], "page-item")
+      previous_or_next_page(num, @options[:next_label], " page-item ")
     end
 
     def ul_class
